bf4819eba2a0a805026c9415051e3f0fc93665f4
Chris Chamberlain <chrisbc@artisan.co.nz>
2020-10-15 07:41:09 +1300

    fixed missing config setter

--- src/python/automation/run_rupture_sets.py
+++ src/python/automation/run_rupture_sets.py
@@ -11,7 +11,7 @@ inputs = {"ALL": "./data/FaultModels/DEMO2_DIPFIX_crustal_opensha.xml",
 strategies = ['DOWNDIP', 'POINTS', 'UCERF3']
 jump_limits = [0.5, 0.75, 1.0, 2.0, 3.0, 4.0, 5.0, 5.1, 5.2]
 ddw_ratios = [0.5, 1.0, 1.5, 2.0, 2.5]
-max_sections = 10
+max_sections = 1000
 
 def run_tests(inputs, jump_limits, ddw_ratios):
     for key, filename in inputs.items():
@@ -23,6 +23,7 @@ def run_tests(inputs, jump_limits, ddw_ratios):
                     builder.setMaxFaultSections(max_sections)\
                         .setMaxJumpDistance(distance)\
                         .setPermutationStrategy(strategy)\
+                        .setMaxSubSectionLength(ddw)\
                         .buildRuptureSet(filename)
                     builder.writeRuptureSet("/tmp/TEST_ruptureset_ddw(%s)_jump(%s)_%s_%s.zip" % (ddw, distance, key, strategy))
                     print("built & saved in %s" % str(dt.utcnow() - t0))
