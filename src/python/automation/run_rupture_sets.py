7d1e587d90350ca5801df8969637958ed97616d8
Chris Chamberlain <chrisbc@artisan.co.nz>
2020-10-15 10:03:12 +1300

    extract metrics from builder

--- src/python/automation/run_rupture_sets.py
+++ src/python/automation/run_rupture_sets.py
@@ -1,6 +1,6 @@
 from py4j.java_gateway import JavaGateway
 from datetime import datetime as dt
-
+import json
 gateway = JavaGateway()
 app = gateway.entry_point
 
@@ -11,7 +11,24 @@ inputs = {"ALL": "./data/FaultModels/DEMO2_DIPFIX_crustal_opensha.xml",
 strategies = ['DOWNDIP', 'POINTS', 'UCERF3']
 jump_limits = [0.5, 0.75, 1.0, 2.0, 3.0, 4.0, 5.0, 5.1, 5.2]
 ddw_ratios = [0.5, 1.0, 1.5, 2.0, 2.5]
-max_sections = 1000
+max_sections = 100
+
+def ruptureSetMetrics(builder):		
+	conf = builder.getPlausabilityConfig()
+	
+	metrics = {}
+	metrics["sub_section_count"] = builder.getSubSections().size()
+	metrics["rupture_count"] = builder.getRuptures().size()
+	metrics["possible_cluster_connections_count"] = conf.getConnectionStrategy().getClusterConnectionCount()
+	
+	# get info form the configuratiion
+	conf_diags = json.loads(conf.toJSON())
+	conns = 0
+	for cluster in conf_diags['connectionStrategy']['clusters']: 
+		conns += len(cluster.get('connections',[]))
+	metrics["cluster_connections_count"] = conns
+	
+	return metrics
 
 def run_tests(inputs, jump_limits, ddw_ratios):
     for key, filename in inputs.items():
@@ -25,6 +42,7 @@ def run_tests(inputs, jump_limits, ddw_ratios):
                         .setPermutationStrategy(strategy)\
                         .setMaxSubSectionLength(ddw)\
                         .buildRuptureSet(filename)
+                    print(ruptureSetMetrics(builder))
                     builder.writeRuptureSet("/tmp/TEST_ruptureset_ddw(%s)_jump(%s)_%s_%s.zip" % (ddw, distance, key, strategy))
                     print("built & saved in %s" % str(dt.utcnow() - t0))
 
